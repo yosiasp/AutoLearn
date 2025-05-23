a78c245c20175e92ceaf25e1f2c2da611f7cc766
Valentino812 <untarvalen123@gmail.com>
2025-05-23 22:37:43 +0700

    changing back button icon

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -109,8 +109,8 @@ const LoginForm = () => {
       {/* Back Button */}
       <div className="back-button" onClick={() => navigate('/')}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M19 12H5" stroke="#FAF9F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-          <path d="M12 19L5 12L12 5" stroke="#FAF9F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M3 9L12 2L21 9V20C21 20.5304 20.7893 21.0391 20.4142 21.4142C20.0391 21.7893 19.5304 22 19 22H5C4.46957 22 3.96086 21.7893 3.58579 21.4142C3.21071 21.0391 3 20.5304 3 20V9Z" stroke="#FAF9F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M9 22V12H15V22" stroke="#FAF9F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
       </div>
       {/* Logo */}
