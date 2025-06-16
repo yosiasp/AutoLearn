9aacfd808c4a404f6ccc1d13775b878a8676f36c
Valentino812 <untarvalen123@gmail.com>
2025-06-16 22:08:56 +0700

    title for overview

--- frontend/src/Overview.js
+++ frontend/src/Overview.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Overview.css';
 
 const Overview = () => {
+  // Title
+  useEffect(() => {
+    document.title = "Autolearn";
+  }, []);
+
   const navigate = useNavigate();
 
   return (
